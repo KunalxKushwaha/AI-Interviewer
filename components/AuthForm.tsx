fb00253865efde6adc88fce6d528919d0163813a
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-10-02 15:15:22 +0530

    Making Auth Page

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -14,8 +14,7 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-
-
+import Image from "next/image";
 
 const formSchema = z.object({
     username: z.string().min(2).max(50),
@@ -38,7 +37,15 @@ const AuthForm = () => {
   }
     
   return (
-    <div>
+    <div className='card-border lg:min-w-[566px]'>
+        <div className='flex flex-col gap-6 card py-14 px-10'>
+            <div className='flex flex-row justify-center gap-2'>
+                <Image src ="/logo.svg" alt ="logo" height ={32} width={38}/>
+                <h2 className='text-primary-100'>HireVision</h2>
+
+            </div>
+
+        </div>
          <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
