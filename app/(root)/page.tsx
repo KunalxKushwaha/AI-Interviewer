dab03a0a64a4e88f9ad6bf2db566597ddf012d9d
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-10-03 23:16:14 +0530

    Making Nav

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Button } from "@/components/ui/button"
+
 
 const page = () => {
   return (
-    <div>
-      Home
-    </div>
+    <>
+      <section className='card-cta'>
+        <div className='flex flex-col gap-6 max-w-lg'>
+          <h2>Get Interview Ready with AI Powered Practice & Feedback. </h2>
+          <p className='text-lg'>Practice on Real Interview Questions & get Instant Feedback.</p>
+          
+          <Button asChild className='btn-primary max-sm:w-full'></Button>
+
+        </div>
+      </section>
+    </>
   )
 }
 
