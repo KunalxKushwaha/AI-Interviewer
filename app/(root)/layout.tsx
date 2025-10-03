274d5355b4d97e236f575aaf74651eff155b0585
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-10-03 22:53:21 +0530

    Making Nav

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,18 @@
+import Image from 'next/image'
+import  Link  from 'next/link'
 import React from 'react'
 
 const Rootlayout = ({children}: {children: React.ReactNode}) => {
   return (
-    <div>
-      {children}
+    <div className='root-layout'>
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+        <Image src="/logo.svg" alt="logo" width={38} height={32} />
+        <h2 className='text-primary-100'>HireVision</h2>
+        </Link>
+
+       
+      </nav>
     </div>
   )
 }
