a544feecf6d8f692d0445b69d1cbcfcb4af548f4
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-10-07 14:41:18 +0530

    Image logos added

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import dayjs from 'dayjs'
+import Image from 'next/image';
+import { getRandomInterviewCover } from '@/lib/utils';
 
 const InterviewCard = ({interviewId, userId, role, type, techstack, createdAt} : InterviewCardProps) => {
   const feedback = null as Feedback | null;
@@ -12,6 +14,8 @@ const InterviewCard = ({interviewId, userId, role, type, techstack, createdAt} :
           <div className='absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg bg-light-600'>
             <p className='badge-text'>{normalizedType}</p>
           </div>
+
+          <Image src={getRandomInterviewCover()} alt='Interview Cover image' width={90} height={90} className='rounded-full object-fit size-[90px]'/>
         </div>
 
 
