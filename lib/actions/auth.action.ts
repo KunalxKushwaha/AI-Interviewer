cbd4c26d04aca03e17fdc88fe9ac2f5572dd9a28
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-10-25 19:23:10 +0530

    Making Sign-in-out page Functional

---
+++ lib/actions/auth.action.ts
@@ -0,0 +1,26 @@
+'use server';
+
+import { success } from "zod";
+
+export async function signUp(params: SignUpParams) {
+    const {uid, name, email} = params;
+
+    try {
+        
+    } catch (error: any) {
+        console.error('Error Creating a User', error);
+
+        if(error.code === 'auth/email-already-exists') {
+            return {
+                success: false,
+                message: 'Email already in use'
+            }
+        }
+
+        return {
+            success: false,
+            message: 'Could not create user'
+        }
+        
+    }
+}
