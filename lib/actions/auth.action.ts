d107e346b8230fc5bbf9c3998b708f504b0d27e8
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-10-27 21:27:31 +0530

    Making Sign-in-out page Functional

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -19,7 +19,7 @@ export async function signUp(params: SignUpParams) {
 
         return {
             success: false,
-            message: 'Could not create user'
+            message: 'Could not create a user'
         }
         
     }
